f1ae1b44f5c5f1dd047c09295b49f28f26797281
abhijeetr786 <abhijeetrakshit786@gmail.com>
2024-05-21 14:00:59 +0530

    hii

---
+++ tutorial.js
@@ -0,0 +1,23 @@
+console.log("this is javascript tutorial");
+
+function greet(name){
+    console.log(name + " is a good boy");
+
+}
+let name ="abhi";
+let name1 ="vitu";
+let name2 ="sauraya";
+let  name3="bansi";
+let name4="saami"
+let name5="anis";
+let name6="kajal"
+greet(name);
+greet(name1);
+greet(name2);
+greet(name3);
+greet(name4);
+greet(name5);
+greet(name6);
+
+
+
